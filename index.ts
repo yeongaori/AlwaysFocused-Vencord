4e177b9f0718d91552d4151790489dc77b000c37
yeongaori <yeongaori@yeongaori.com>
2026-06-14 19:33:06 +0900

    Dropped interceptor

--- index.ts
+++ index.ts
@@ -1,20 +1,6 @@
 import definePlugin from "@utils/types";
-import { FluxDispatcher, WindowStore } from "@webpack/common";
+import { WindowStore } from "@webpack/common";
 
-let active = false;
-let interceptorAdded = false;
-
-const block = (e: Event) => {
-    if (!e.isTrusted) return;
-    e.stopImmediatePropagation();
-};
-
-const interceptor = (action: any) => {
-    if (!active) return false;
-    return action?.type === "WINDOW_UNFOCUS";
-};
-
-let originalHasFocus: typeof document.hasFocus;
 let originalIsFocused: (() => boolean) | undefined;
 let originalIsVisible: (() => boolean) | undefined;
 
@@ -24,40 +10,21 @@ export default definePlugin({
     authors: [{ name: "yeongaori", id: 602447697047191562n }],
 
     start() {
-        active = true;
-
-        originalHasFocus = document.hasFocus.bind(document);
-        document.hasFocus = () => true;
-
         const store = WindowStore as any;
         originalIsFocused = store.isFocused?.bind(store);
         originalIsVisible = store.isVisible?.bind(store);
+
         store.isFocused = () => true;
         if (originalIsVisible) store.isVisible = () => true;
 
-        if (!interceptorAdded) {
-            (FluxDispatcher as any).addInterceptor(interceptor);
-            interceptorAdded = true;
-        }
-
-        window.addEventListener("blur", block, true);
-        document.addEventListener("visibilitychange", block, true);
-        document.addEventListener("webkitvisibilitychange", block, true);
-
-        FluxDispatcher.dispatch({ type: "WINDOW_FOCUS", focused: true } as any);
+        store.emitChange?.();
     },
 
     stop() {
-        active = false;
-
-        if (originalHasFocus) document.hasFocus = originalHasFocus;
-
         const store = WindowStore as any;
         if (originalIsFocused) store.isFocused = originalIsFocused;
         if (originalIsVisible) store.isVisible = originalIsVisible;
 
-        window.removeEventListener("blur", block, true);
-        document.removeEventListener("visibilitychange", block, true);
-        document.removeEventListener("webkitvisibilitychange", block, true);
+        store.emitChange?.();
     }
 });
